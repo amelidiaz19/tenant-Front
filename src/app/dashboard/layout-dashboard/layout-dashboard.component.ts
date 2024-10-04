b95989f63bd80bf17080109769bf39acbc393629
amelidiaz19 <diazchavez1905@gmail.com>
2024-10-03 20:40:08 -0500

    initflowbite

--- src/app/dashboard/layout-dashboard/layout-dashboard.component.ts
+++ src/app/dashboard/layout-dashboard/layout-dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-layout-dashboard',
@@ -8,6 +9,12 @@ import { RouterLink, RouterOutlet } from '@angular/router';
   templateUrl: './layout-dashboard.component.html',
   styles: ``
 })
-export class LayoutDashboardComponent {
+export class LayoutDashboardComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 
 }
