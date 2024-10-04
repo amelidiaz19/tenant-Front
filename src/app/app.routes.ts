2ea8f823191b7a493db8f8c265beba5be7c6cc72
PaulPPS632 <jeffertsap@hotmail.com>
2024-10-03 23:19:43 -0500

    agregando components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,25 +2,25 @@ import { Routes } from '@angular/router';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
-    {
-      path: '',
-      loadChildren: () =>
-        import('./home/home.routes').then((m) => m.HomeRoutesModule),
+  {
+    path: '',
+    loadChildren: () =>
+      import('./home/home.routes').then((m) => m.HomeRoutesModule),
+  },
+  {
+    path: 'dashboard',
+    //canActivate: [authGuard],
+    loadChildren: () =>
+      import('./dashboard/dashboard.routes').then(
+        (m) => m.DashboardRoutesModule
+      ),
+    data: {
+      title: 'Dashboard',
+      roles: ['administrador', 'encargado'],
     },
-    {
-      path: 'dashboard',
-      //canActivate: [authGuard],
-      loadChildren: () =>
-        import('./dashboard/dashboard.routes').then(
-          (m) => m.DashboardRoutesModule
-        ),
-      data: {
-        title: 'Dashboard',
-        roles: ['administrador', 'encargado'],
-      },
-    },
-    {
-      path: '**',
-      redirectTo: '',
-    },
-  ];
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
