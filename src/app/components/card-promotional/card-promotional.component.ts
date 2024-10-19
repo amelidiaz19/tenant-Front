aaa3e4e1ea31946c157f5262c543cdfec21f4939
amelidiaz19 <diazchavez1905@gmail.com>
2024-10-19 16:54:04 -0500

    homepage update

--- src/app/components/card-promotional/card-promotional.component.ts
+++ src/app/components/card-promotional/card-promotional.component.ts
@@ -1,13 +1,64 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+
+interface Beneficio {
+  descripcion: string;
+  incluido: boolean;
+}
 
 @Component({
   selector: 'app-card-promotional',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './card-promotional.component.html',
   styles: ``,
 })
-export class CardPromotionalComponent {
-  @Input() Tipo: string = 'Standar';
+export class CardPromotionalComponent implements OnInit {
+  @Input() tipo: 'Standard' | 'Pro' | 'Custom' = 'Standard';
+  @Input() precio: number = 0;
+
+  beneficios: Beneficio[] = [];
+
+  ngOnInit(): void {
+    this.setBeneficios();
+  }
+
+  private setBeneficios() {
+    const beneficiosComunes = [
+      { descripcion: 'Acceso a funciones básicas', incluido: true },
+      { descripcion: 'Soporte por correo electrónico', incluido: true },
+    ];
+
+    switch (this.tipo) {
+      case 'Standard':
+        this.beneficios = [
+          ...beneficiosComunes,
+          { descripcion: 'Hasta 5 usuarios', incluido: true },
+          { descripcion: 'Almacenamiento 10GB', incluido: true },
+          { descripcion: 'Integraciones avanzadas', incluido: false },
+          { descripcion: 'Soporte prioritario', incluido: false},
+        ];
+        break;
+      case 'Pro':
+        this.beneficios = [
+          ...beneficiosComunes,
+          { descripcion: 'Hasta 20 usuarios', incluido: true },
+          { descripcion: 'Almacenamiento 50GB', incluido: true },
+          { descripcion: 'Integraciones avanzadas', incluido: true },
+          { descripcion: 'Soporte prioritario', incluido: true },
+        ];
+        break;
+      case 'Custom':
+        this.beneficios = [
+          ...beneficiosComunes,
+          { descripcion: 'Usuarios ilimitados', incluido: true },
+          { descripcion: 'Almacenamiento personalizado', incluido: true },
+          { descripcion: 'Integraciones personalizadas', incluido: true },
+          { descripcion: 'Soporte 24/7', incluido: true },
+          { descripcion: 'Gestor de cuenta dedicado', incluido: true },
+        ];
+        break;
+    }
+  }
 }
